15f454f61d24b0556f88656c55068693cd6d2acc
rvthless <40124487+rvthless@users.noreply.github.com>
2021-02-10 21:03:28 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,18 +3,30 @@ import Header from './Header'
 import { Box, Container } from '@material-ui/core'
 import styled, { ThemeProvider } from 'styled-components';
 import { compose, css } from '@material-ui/system';
-
+import Head from 'next/head'
+ 
 type Props = {
-	children: ReactNode
+    children: ReactNode
 }
-
+ 
 const Layout: React.FC<Props> = (props) => (
-	<Box  css={{ width: '80%', margin: 'auto' }}>
-		<Header />
-		<Container className="layout" maxWidth="md">
-			{props.children}
-		</Container>
-	</Box>
+ 
+<Head>
+  <style> {globalStyle}
+    <Box  css={{ bgcolor: '#FFFFFF', width: '80%', margin: 'auto' }}>
+        <Header />
+        <Container className="layout" maxWidth="md">
+            {props.children}
+        </Container>
+    </Box>
+</style>
+</Head>
 )
-
+ 
+const globalStyle = `
+body {
+    background-color: #D1C7AF;
+}
+`
+ 
 export default Layout
