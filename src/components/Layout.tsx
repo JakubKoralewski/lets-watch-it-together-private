edcf4759c9e3b040fd015f5b3e04b1e258585b87
rvthless <40124487+rvthless@users.noreply.github.com>
2021-02-10 20:19:15 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import React, { ReactNode } from 'react'
 import Header from './Header'
 import { Box, Container } from '@material-ui/core'
+import styled, { ThemeProvider } from 'styled-components';
+import { compose, css } from '@material-ui/system';
 
 type Props = {
 	children: ReactNode
 }
 
 const Layout: React.FC<Props> = (props) => (
-	<Box>
+	<Box  css={{ width: '80%', margin: 'auto' }}>
 		<Header />
-		<Container className="layout">
+		<Container className="layout" maxWidth="md">
 			{props.children}
 		</Container>
 	</Box>
